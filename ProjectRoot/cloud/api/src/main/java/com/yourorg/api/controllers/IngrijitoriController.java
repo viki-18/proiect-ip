2dae70377841bf4c971ae9e0d83ee9b532663f76
alexgh05 <alex.ghenciu05@gmail.com>
2025-04-17 09:46:53 +0300

    Working CLoud

--- ProjectRoot/cloud/api/src/main/java/com/yourorg/api/controllers/IngrijitoriController.java
+++ ProjectRoot/cloud/api/src/main/java/com/yourorg/api/controllers/IngrijitoriController.java
@@ -1,34 +1,48 @@
 package com.yourorg.api.controllers;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.*;
+import com.yourorg.api.model.Ingrijitor;
+import com.yourorg.api.service.IngrijitorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ingrijitori")
 public class IngrijitoriController {
 
+    @Autowired
+    private IngrijitorService service;
+
     @GetMapping
-    public List<String> getAllIngrijitori() {
-        return new ArrayList<>();
+    public List<Ingrijitor> getAllIngrijitori() {
+        return service.getAllIngrijitori();
     }
 
     @GetMapping("/{id}")
-    public String getIngrijitorById(@PathVariable int id) {
-        return "Ingrijitor " + id;
+    public Optional<Ingrijitor> getIngrijitorById(@PathVariable Long id) {
+        return service.getIngrijitorById(id);
     }
 
     @PostMapping
-    public String createIngrijitor(@RequestBody String ingrijitor) {
-        return "Created ingrijitor";
+    public Ingrijitor createIngrijitor(@RequestBody Ingrijitor ingrijitor) {
+        return service.createIngrijitor(ingrijitor);
     }
 
     @PutMapping("/{id}")
-    public String updateIngrijitor(@PathVariable int id, @RequestBody String ingrijitor) {
-        return "Updated ingrijitor " + id;
+    public Ingrijitor updateIngrijitor(@PathVariable Long id, @RequestBody Ingrijitor ingrijitor) {
+        return service.updateIngrijitor(id, ingrijitor);
     }
 
     @DeleteMapping("/{id}")
-    public String deleteIngrijitor(@PathVariable int id) {
-        return "Deleted ingrijitor " + id;
+    public void deleteIngrijitor(@PathVariable Long id) {
+        service.deleteIngrijitor(id);
     }
 }
